54b537ed82207284a40e54c206b5e9d5eb58f2ad
gasangw <thomasgasangwa@gmail.com>
2023-06-21 08:24:32 +0200

    Implement the nav section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,18 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+    fontFamily: {
+      'display': ['Poppins', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [],
