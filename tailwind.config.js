a03aee79ea9cd7f9b24a0d5eb04b36afbb9d9a22
gasangw <thomasgasangwa@gmail.com>
2023-06-21 14:54:05 +0200

    Improve styling for the cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,9 @@ module.exports = {
       padding: {
         DEFAULT: '1rem',
         sm: '2rem',
-        lg: '3rem',
-        xl: '3.5rem',
-        '2xl': '4rem',
+        lg: '5rem',
+        xl: '5.5rem',
+        '2xl': '6rem',
       },
     },
     fontFamily: {
